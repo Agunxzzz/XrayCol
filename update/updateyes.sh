7a4af505632451dd3a664383f9d33eb8f0d8e317
Agunxzzz <86614702+Agunxzzz@users.noreply.github.com>
2023-03-23 20:12:58 +0700

    Update updateyes.sh

--- update/updateyes.sh
+++ update/updateyes.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo -e ngetes update bang
+echo -e Proses Update bang
+#hapus lawas
 rm -f /root/versi
+rm -f /usr/bin/running
+#download yang baru
+wget -q -O /usr/bin/running "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/update/data/Urunning.sh"
 wget -q -O /root/versi "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/update/versiupdate"
+#Permission
+chmod +x /usr/bin/running
 echo -e "\e[32mUpdate Selesai...\e[0m"
 cd
