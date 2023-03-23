97c2e1ebb0cf9f4e7d2046635dc3f1f03e4fe1c9
Agunxzzz <86614702+Agunxzzz@users.noreply.github.com>
2023-03-23 12:02:48 +0700

    Update updateyes.sh

--- update/updateyes.sh
+++ update/updateyes.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+echo -e ngetes update bang
+rm -f /root/versi
+wget -q -O /root/versi "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/update/versiupdate"
+echo -e "\e[32mUpdate Selesai...\e[0m"
+cd
